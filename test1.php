88c2b2c8e0610d69939ca5d8aeb807985e580114
unknown <shahshashi8@gmail.com>
2019-04-23 00:35:59 +0530

    hgahg

---
+++ test1.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+$n = 10 ;
+$num1 = 0 ;
+$num2 = 1;
+
+for($i=0 ; $i<$n; $i++)
+{
+	
+
+   echo $num1."<br>";
+   $num3 = $num1+ $num2 ;
+   $num1 = $num2 ;
+   $num2 = $num3;
+
+
+
+
+}
+
+
+
+
+
+
+?>
